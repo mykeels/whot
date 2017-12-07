36e49db0bbe151fc39b6db148e291e497735147a
Ikechi Michael <mykehell123@gmail.com>
2017-12-07 11:39:38 +0100

    feat: add shapes

---
+++ src/shapes.js
@@ -0,0 +1,12 @@
+/**
+ * shapes determine the type of a card
+ */
+
+export default {
+    Circle: 'Circle',
+    Triangle: 'Triangle',
+    Cross: 'Cross',
+    Square: 'Square',
+    Star: 'Star',
+    Whot: 'Whot'
+}
