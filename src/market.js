0d4d3945ac4bb78e749b63c07e319878f69b082f
Ikechi Michael <mykehell123@gmail.com>
2017-12-10 09:48:20 +0100

    feat: market now emitting events

--- src/market.js
+++ src/market.js
@@ -4,7 +4,7 @@ const Shapes = require("../src/shapes")
 const createError = require('./errors')
 const { createTypeError } = require('./errors')
 const EventEmitter = require('events').EventEmitter
-const logger = require('./logger')('player.js')
+const logger = require('./logger')('market.js')
 
 const OutOfRangeError = createError('OutOfRangeError')
 
@@ -13,9 +13,18 @@ const OutOfRangeError = createError('OutOfRangeError')
  * @param {Object} props
  * @param {Number} props.noOfDecks how many card decks should be in the market?
  * @param {Function} props.pile returns Pile instance
+ * @param {EventEmitter} props.emitter
+ * 
+ * @event market:create
+ * @event market:pick
  */
 const Market = function (props = {}) {
-    props.noOfDecks = props.noOfDecks || 1 
+    props.noOfDecks = props.noOfDecks || 1
+
+    if (!props.emitter) {
+        logger.warn('props.emitter not defined')
+        props.emitter = new EventEmitter()
+    }
 
     let cards = []
 
@@ -23,6 +32,8 @@ const Market = function (props = {}) {
         const deck = new Deck()
         deck.shuffle().forEach(card => cards.push(card))
     }
+
+    props.emitter.emit('market:create', cards.slice(0))
 
     this.pick = (no = 1) => {
         if (no >= cards.length) {
@@ -33,7 +44,9 @@ const Market = function (props = {}) {
                 cards = cards.sort((a, b) => Math.random() - 0.5)
             }
         }
-        return cards.splice(0, no)
+        const pickedCards = cards.splice(0, no)
+        props.emitter.emit('market:pick', pickedCards)
+        return pickedCards
     }
 
     this.count = () => cards.length
