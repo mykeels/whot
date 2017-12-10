867b0ca0eff93f8d8a2c82c5ed8f2ef3e1de99b6
Ikechi Michael <mykehell123@gmail.com>
2017-12-10 09:42:37 +0100

    feat: deck emits events

--- src/deck.js
+++ src/deck.js
@@ -1,4 +1,6 @@
 const Card = require("./card")
+const logger = require('./logger')('deck.js')
+const EventEmitter = require('events').EventEmitter
 const {
     GetCircle,
     GetStar,
@@ -17,17 +19,33 @@ const squares = [1, 2, 3, 5, 7, 10, 11, 13, 14]
 const stars = [1, 2, 3, 4, 5, 7, 8]
 const whots = [20, 20, 20, 20, 20]
 
-function Deck() {
-  this.cards = [].concat(circles.map(value => (GetCircle({ value }))))
+/**
+ * @param {Object} props
+ * @param {EventEmitter} props.emitter
+ * 
+ * @event deck:create
+ * @event deck:shuffle
+ */
+function Deck(props = {}) {
+    if (!props.emitter) {
+        logger.warn('props.emitter not defined')
+        props.emitter = new EventEmitter()
+    }
+
+    this.cards = [].concat(circles.map(value => (GetCircle({ value }))))
                 .concat(triangles.map(value => (GetTriangle({ value }))))
                 .concat(crosses.map(value => (GetCross({ value }))))
                 .concat(squares.map(value => (GetSquare({ value }))))
                 .concat(stars.map(value => (GetStar({ value }))))
                 .concat(whots.map(value => (GetWhot({ value }))))
 
-  this.shuffle = () => {
-      return this.cards.map((card, index) => index).sort((a, b) => Math.random() - 0.5).map(index => this.cards[index])
-  }
+    props.emitter.emit('deck:create', this.cards)
+
+    this.shuffle = () => {
+        const cards = this.cards.map((card, index) => index).sort((a, b) => Math.random() - 0.5).map(index => this.cards[index])
+        props.emitter.emit('deck:shuffle', cards)
+        return cards
+    }
 }
 
 module.exports = Deck
